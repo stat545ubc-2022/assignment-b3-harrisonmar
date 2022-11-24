1fffaf90c3387b008836462b81714ef534dbb12c
Harrison Mar <harrisonmar95@gmail.com>
2022-11-24 12:53:11 -0800

    initial commit of shiny app

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# assignment-b3-harrisonmar
+# Assignment B3 Harrison Mar
+
+## Assignment Information
+
+Option A: Modifying BCL Shiny App started in class
+
+## Updates to BCL App
+
+I made a number of changes to the basic version of the BCL app which include the following:
+
+- **Shiny Dashboard** : changed the layout/appearance of the initial shiny app to a shiny dashboard containing a Dashboard/Homepage tab, Charts tab, and Table tab
+- **Aesthetics** : changed the colour pallet of the shiny dashboard from the default colour scheme
+- **Title and Image** : Added a title to the dashboard page which is clickable and takes users to the BCL website. Also included a small image in the header/title as well
+- **Dropdown Menu** : Added a dropdown widget feature to the control panel in the Charts tab which allows users to filter for country on top of the initial price and beverage type filters
+- **Interactive Data Table** : Used the DT package to create an interactive data table located in the Table tab which allows users to use keyword search and order columns in ascending or descending order
+
+The running instance of the Shiny App can be found here:
+
+https://harrisonmar.shinyapps.io/assignment-b3-harrisonmar/
+
+The original data for this app can be found here:
+
+https://github.com/daattali/shiny-server/tree/master/bcl
+
+## Repository Files
+
+- **README** : overview of app and repository contents
+- **app.R** : code for operational shiny app
+- **bcl-data.csv** : original data file 
+- **www** : contains image used in dashboard title header
+- **rsconnect** : file for deploying app on shinyapps.io
+- **.gitignore** : indicates which files git should ignore when committing and pushing
+- **.Rproj** : details the project settings and is a shortcut for opening the project directly from the filesystem
---
+++ app.R
@@ -0,0 +1,146 @@
+library(shiny)
+library(tidyverse)
+library(shinydashboard)
+library(DT)
+
+# reading in bcl data
+bcl <- read.csv(here::here("bcl-data.csv"))
+
+
+
+# adding a title component to the shiny dashboard header feature, including a picture and a clickable
+# titles that will take users to the BCL webpage
+title <- tags$a(
+  href="https://www.bcliquorstores.com/",
+  tags$img(src="cartoonliquor.png", height="50", width="50"),
+  "BC Liquor Stores Data"
+)
+
+# order in which arguments are passed here defines in what order they will appear spatially
+# on the webpage
+ui <- fluidPage(
+  
+  # adjusting color aesthetic of the page and applying title created with hyperlink and image
+  dashboardPage(skin = "green",
+     dashboardHeader(title = h4(title),
+                     titleWidth = 250
+    )
+    ,
+    dashboardSidebar(
+  # creating menu items in the sidebar of the dashboard for ease of navigation to view data tables or charts
+      width = 250,
+      sidebarMenu(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Charts", tabName = "charts", icon = icon("bar-chart-o")),
+        menuItem("Table", tabName = "table", icon = icon("table"))
+        )
+    )
+    ,
+    dashboardBody(
+  # populating the tab/each menu item with text and respective content
+      tabItems(
+        tabItem(tabName = "dashboard",
+          h1(strong("Welcome to the BC Liquor Data Visualization Dashboard!", style = "font-si40pt"))
+          ,
+          br()
+          ,
+          tags$div("This Shiny Dashboard has been created as an assignment for UBC's STAT 545B course. Students are tasked with building and deploying a Shiny App where they must demonstrate their ability to 
+            display clear data findings, along with a clean and useful webpage UI. This page has been created using ",
+                   tags$a(href="https://shiny.rstudio.com/",
+                          "shiny"),
+                   "and",
+                   tags$a(href="hhttps://rstudio.github.io/shinydashboard/",
+                          "shiny dashboard"),
+                   "."
+                   )
+          ,
+          br()
+          ,
+          p("Use the tabs on the left hand side of the page for navigation. Each tab accesses a different form of visualizaiton.")
+          ,
+          br()
+          ,
+          a(href = "https://github.com/daattali/shiny-server/tree/master/bcl", 
+           "Link to Original Dataset")
+      )
+      ,
+      tabItem(tabName = "charts",
+              h2(strong("BC Liquor Data : Histrogram Visualization")),
+              br(),
+              p("Using the Control panel on the right side of the page, filter data sorted by Alcohol Content based on price range,
+                type of product, and country of origin."),
+              br(),
+              fluidRow(
+                # putting the histogram and control panel into respective "boxes" on the dashboard
+                box(
+                  plotOutput("alcohol_hist"))
+                ,
+                box(
+                  title = "Controls",
+                  sliderInput("priceInput", "Price", 0, 100,
+                              value = c(25,40), pre = "$"),
+                  radioButtons("typeInput", "Type", 
+                               choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE")),
+                  # adding select bar widget such that users can choose to further filter histogram output by country
+                  selectInput("v_select", label = "Country", choices = unique(bcl$Country), selected = "Canada"))
+                )
+              )
+      ,
+      tabItem(tabName = "table",
+              h2(strong("BC Liquor Data : Data Table")),
+              br(),
+              p("Sort columns in ascending or descending order(alphabetically or numerically depending on column type) or
+                search by keyword using the search bar located at the top right of the data table."),
+              br(),
+              # output entire data table to a new tab on the dashboard using the "DT" library, includes the ability to order
+              # columns in ascending or descending order numerically or alphabetically (depending on column type) and search
+              # by keyword 
+              fluidRow(
+                  DTOutput("mytable1")
+               )
+              )
+        
+       )
+      )
+    )
+  )
+  
+
+server <- function(input, output) {
+  
+  # must include reactive if the object must change with user inputs
+  filtered_data <-  
+    reactive(bcl %>% filter(Price > input$priceInput[1] &
+                                          Price < input$priceInput[2] &
+                                          Type == input$typeInput))
+
+  
+  # must now add round brackets to whatever reactive variable is in the code to access
+  output$alcohol_hist <- 
+    renderPlot({
+      filtered_data() %>%
+      # adding additional filter to go along with the select bar widget
+      filter(Country == input$v_select) %>%
+      ggplot(aes(Alcohol_Content)) + 
+        geom_histogram(color = "darkblue", fill = "lightblue") +
+        ggtitle("Count vs Alcohol Content") +
+        theme_minimal() +
+        xlab("Alcohol Content (%)") +
+        ylab("Count")
+      })
+  
+  
+  output$data_table <- 
+    renderTable({
+      filtered_data()
+      })
+  
+  # new full data table to be displayed using the "DT" library
+  output$mytable1 <-
+    DT::renderDataTable({
+      bcl
+    })
+    
+}
+
+shinyApp(ui = ui, server = server)
